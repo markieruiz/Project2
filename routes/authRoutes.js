cb13dd6a2596b4f0744685658c0da66d166002db
Speedway <harpalassi@gmail.com>
2019-02-04 23:08:11 -0600

    logout working

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,15 @@ var router = require('express').Router();
 var db = require("../models");
 var passport = require('passport');
 
-
+function authCheck (req, res, next) {
+    if(!req.user) {
+        //if user is not logged in
+        res.redirect("/auth/login");
+    } else {
+        // if logged in
+        next();
+    }
+}
 // auth login 
 router.get('/login', (req, res) => {
     res.render("index");
@@ -11,7 +19,8 @@ router.get('/login', (req, res) => {
 // auth logout
 router.get('/logout', (req, res) => {
     //handle with passport
-    res.send("logging out")
+    req.logout();
+    res.redirect("example")
 })
 
 //auth with google
@@ -21,7 +30,8 @@ router.get('/google', passport.authenticate('google', {
 
 //callback route for google to redirect to
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-    res.send(req.user)
+    // res.send(req.user)
+    res.send(`you are logged in! this is your profile, ${req.user.firstName} ${req.user.lastName}`)
 })
 
 module.exports = router;
