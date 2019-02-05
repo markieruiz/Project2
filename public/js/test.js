2ce6b98038701d762c8b4ee3bee2fb4ab5dd6a38
jfecke <jfecke@gmail.com>
2019-02-04 21:21:16 -0600

    Got everything working again after merge

--- public/js/test.js
+++ public/js/test.js
@@ -1,137 +1,127 @@
 $(document).ready(function() {
-
   $("#test").on("click", function(event) {
-      event.preventDefault();
-      filterSearch();
+    event.preventDefault();
+    filterSearch();
   });
-  
+
   function filterSearch() {
-      var checks = $(".sports");
-      var starttime = $("#starttime").val();
-      var mysports = [];
-      var testdate = new Date();
-      for (let i = 0; i < 3; i++) {
-          if (checks[i].checked == true) {
-              mysports.push(checks[i].id);
-          }
+    var checks = $(".sports");
+    var starttime = $("#starttime").val();
+    var mysports = [];
+    for (var i = 0; i < 3; i++) {
+      if (checks[i].checked === true) {
+        mysports.push(checks[i].id);
       }
-      var myfilter = {
-          location: $("#myloc").val(),
-          distance: $("#distance").val(),
-          sports: mysports,
-          endtime: starttime
-          };
-      $.ajax({
-          url: "/api/filter",
-          method: "PUT",
-          data: myfilter,
-          dataType: 'json'
-      }).then(function(results) {
-          deleteMarkers();
-          for (i in results) {
-            var marker = new google.maps.Marker({
-                position: { lat: parseFloat(results[i].latitude), lng: parseFloat(results[i].longitude) },
-                map: map
-            });
-            markers.push(marker);
-            var infowindow = new google.maps.InfoWindow({
-                content: '<h4>' + results[i].title + '</h4>' + '<br/>' +
-                    '<p>' + results[i].description + '</p>' + "<button id=' "+ i + "'>Select</button>"
-            });
-            infowindows.push(infowindow);
-            
-        }
-        showMarkers();
-        console.log("finish")
-      });
-  }   
-  
+    }
+    var myfilter = {
+      location: $("#myloc").val(),
+      distance: $("#distance").val(),
+      sports: mysports,
+      endtime: starttime
+    };
+    $.ajax({
+      url: "/api/filter",
+      method: "PUT",
+      data: myfilter,
+      dataType: "json"
+    }).then(function(results) {
+      deleteMarkers();
+      for (i in results) {
+        let marker = new google.maps.Marker({
+          position: {
+            lat: parseFloat(results[i].latitude),
+            lng: parseFloat(results[i].longitude)
+          },
+          map: map
+        });
+        markers.push(marker);
+        let infowindow = new google.maps.InfoWindow({
+          content:
+            "<h4>" +
+            results[i].title +
+            "</h4>" +
+            "<br/>" +
+            "<p>" +
+            results[i].description +
+            "</p>" +
+            "<button id=' " +
+            i +
+            "'>Select</button>"
+        });
+        infowindows.push(infowindow);
+      }
+      showMarkers();
+      console.log("finish");
+    });
+  }
 });
 
 var map;
 var marker = null;
 var markers = [];
 var infowindows = [];
+// eslint-disable-next-line no-unused-vars
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 30.314786, lng: -97.738267},
-      zoom: 12
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 30.314786, lng: -97.738267 },
+    zoom: 12
   });
-  map.addListener('click', function(e) {
-      if (marker != null) {
-          marker.setMap(null);
-      }
-      for (var j=0; j < infowindows.length; j++) {
-        infowindows[j].close();
-      }
-      placeMarkerAndPanTo(e.latLng, map);
+  map.addListener("click", function(e) {
+    if (marker !== null) {
+      marker.setMap(null);
+    }
+    for (let j = 0; j < infowindows.length; j++) {
+      infowindows[j].close();
+    }
+    placeMarkerAndPanTo(e.latLng, map);
   });
-} 
+}
 function placeMarkerAndPanTo(latLng, map) {
-    marker = new google.maps.Marker({
-        position: latLng,
-        map: map
-    });
-    var lat = marker.getPosition().lat();
-    var long =  marker.getPosition().lng();
-    var coords = lat+", "+long;
-    $("#myloc").val(coords);
-    //map.panTo(latLng);
+  marker = new google.maps.Marker({
+    position: latLng,
+    map: map
+  });
+  var lat = marker.getPosition().lat();
+  var long = marker.getPosition().lng();
+  var coords = lat + ", " + long;
+  $("#myloc").val(coords);
+  //map.panTo(latLng);
 }
 
 // Sets the map on all markers in the array.
 function setMapOnAll(map) {
-    for (var i = 0; i < markers.length; i++) {
+  for (let i = 0; i < markers.length; i++) {
     let infowindow = infowindows[i];
     let marker = markers[i];
     markers[i].setMap(map);
 
-    google.maps.event.addListener(marker, 'click', function() {
-        for (var j=0; j < infowindows.length; j++) {
-            infowindows[j].close();
-        }
-        infowindow.open(map, marker);
+    google.maps.event.addListener(marker, "click", function() {
+      for (let j = 0; j < infowindows.length; j++) {
+        infowindows[j].close();
+      }
+      infowindow.open(map, marker);
     });
-    }
+  }
 }
 
 // Removes the markers from the map, but keeps them in the array.
 function clearMarkers() {
-    setMapOnAll(null);
+  setMapOnAll(null);
 }
 
 // Shows any markers currently in the array.
 function showMarkers() {
-    setMapOnAll(map);
+  setMapOnAll(map);
 }
 
 // Deletes all markers in the array by removing references to them.
 function deleteMarkers() {
-    clearMarkers();
-    markers = [];
-    infowindows = [];
-    console.log("cleared", markers);
+  clearMarkers();
+  markers = [];
+  infowindows = [];
+  console.log("cleared", markers);
 }
 //infowindow.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //--------------------------------------------------------------------------
 
