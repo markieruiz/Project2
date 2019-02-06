3c7c306cf0b2e6cb10a7c089bf04eff7cd65d64a
Markie Ruiz <markieruiz@Markies-MacBook-Pro.local>
2019-02-05 19:41:15 -0600

    background

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,11 +7,11 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
+  app.get("/findGame/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
-      res.render("example", {
+      res.render("findGame", {
         example: dbExample
       });
     });
