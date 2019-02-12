e52cf268723fb6c558d4ceb1ebf2c39d01ccef8a
jfecke <jfecke@gmail.com>
2019-02-12 17:54:02 -0600

    added my pick to handlebars page and removed some html routing

--- config/config.js
+++ config/config.js
@@ -3,9 +3,8 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: "35.226.227.18",
-    dialect: "mysql",
-    timezone: "-6:00"
+    host: process.env.DB_HOSTNAME,
+    dialect: "mysql"
   },
   test: {
     username: process.env.DB_USERNAME,
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,35 +6,7 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  //Just for testing
-  app.get("/test", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/test.html"));
-  });
-
-  app.get("/findGame", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/findGame.html"));
-  });
-
-  app.get("/create", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/create.html"));
-  });
-
-  // Load example page and pass in an example by id
-  // app.get("/findGame/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(
-  //     dbExample
-  //   ) {
-  //     res.render("findGame", {
-  //       example: dbExample
-  //     });
-  //   });
-  // });
-  // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
-  });
-
-  app.get("/find", function(req, res) {
-    res.render("findGame");
   });
 };
