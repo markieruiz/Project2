41d78b1480a0d730e604d5fb77f0fedff84efaa0
Markie Ruiz <markieruiz@Markies-MacBook-Pro.local>
2019-02-12 19:42:21 -0600

    heroku

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: "35.226.227.18",
+    host:  process.env.DB_HOSTNAME,
     dialect: "mysql",
     timezone: "-6:00"
   },
